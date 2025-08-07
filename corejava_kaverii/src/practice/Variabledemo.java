d26c2699b3c88c296e844ca1649cd5f0269fc95b
kaver <kaverimurashilli@gmail.com>
2025-08-07 13:06:10 +0530

    second commit

---
+++ corejava_kaverii/src/practice/Variabledemo.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Variabledemo {
+		int a=10;
+		void print()
+		{
+			String msg="hello";
+		
+		}
+    static String message ="hello students";
+    
+    public static void main(String[] args) {
+    	
+    	Variabledemo obj = new Variabledemo();
+    	System.out.println("the value of a is " + obj.a);
+    	obj.print();
+    	System.out.println(message);
+    	
+    }
+    
+}
+
