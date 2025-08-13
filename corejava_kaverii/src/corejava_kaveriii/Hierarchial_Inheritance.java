8ea7711b33ff7f42a8be74691dc0f0e7963b2fa6
kaver <kaverimurashilli@gmail.com>
2025-08-13 22:15:08 +0530

    seventh commit

---
+++ corejava_kaverii/src/corejava_kaveriii/Hierarchial_Inheritance.java
@@ -0,0 +1,36 @@
+package corejava_kaveriii;
+
+class Employee
+{
+	void company()
+	{
+		System.out.println("Company :Tnsif");
+	}
+}
+
+class Developer extends Employee
+{
+	void role()
+	{
+		System.out.println("Role :Software Developer");
+	}
+}
+
+class Tester extends Employee
+{
+	void role() {
+		System.out.println("Role :QA Tester");
+	}
+}
+
+public class Hierarchial_Inheritance {
+	public static void main(String[] args) {
+		Developer dev=new Developer();
+		Tester test=new Tester();
+		dev.company();
+		dev.role();
+		test.company();
+		test.role();
+	}
+}
+
