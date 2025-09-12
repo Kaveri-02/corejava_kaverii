cb866c0a8787bd0ca5b5ac6bc9f382dde896a279
kaver <kaverimurashilli@gmail.com>
2025-09-12 19:33:43 +0530

    16th commit

---
+++ corejava_kaverii/src/corejava_kaveriii/ErrorDemo.java
@@ -0,0 +1,17 @@
+package corejava_kaveriii;
+
+public class ErrorDemo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		int sum=a+b;
+		System.out.println(sum);
+		
+		
+	}
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ corejava_kaverii/src/corejava_kaveriii/WithoutExpection.java
@@ -0,0 +1,12 @@
+package corejava_kaveriii;
+
+public class WithoutExpection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d=0;
+		int a=42/0;
+		System.out.println("will not be printed");
+	}
+
+}
