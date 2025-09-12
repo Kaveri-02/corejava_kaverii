94bdd4eb29679a1b575126ba4b6ee6d986b82490
kaver <kaverimurashilli@gmail.com>
2025-09-12 19:44:31 +0530

    16th commit

---
+++ corejava_kaverii/src/corejava_kaveriii/ArithmaticExceptionDemo.java
@@ -0,0 +1,17 @@
+package corejava_kaveriii;
+
+public class ArithmaticExceptionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number_one=50/5;
+		System.out.println(number_one);
+		int number_two=50/10;
+		System.out.println(number_two);
+		int number_three=50/0;
+		System.out.println(number_three);
+		int number_four=50/25;
+		System.out.println(number_four);
+	}
+
+}
