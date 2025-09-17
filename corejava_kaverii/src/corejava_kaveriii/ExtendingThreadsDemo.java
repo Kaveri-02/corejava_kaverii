1de97a34b7bec4c15c6cdc70cf8d7d43a2e40ef8
kaver <kaverimurashilli@gmail.com>
2025-09-17 20:04:49 +0530

    18th commit

--- corejava_kaverii/src/corejava_kaveriii/ExtendingThreadsDemo.java
+++ corejava_kaverii/src/corejava_kaveriii/ExtendingThreadsDemo.java
@@ -23,11 +23,14 @@ public class ExtendingThreadsDemo {
 	public static void main(String[] args) {
 		Eclipse obj = new Eclipse();
 		obj.start();
+		obj.setPriority(Thread.MAX_PRIORITY);//10
 		OneNote obj1 = new OneNote();
 		obj1.start();
+		obj.setPriority(Thread.MIN_PRIORITY);//5
 		Chrome obj2 = new Chrome();
 		obj2.start();
-
+		obj.setPriority(Thread.NORM_PRIORITY);//1
+		
 
 
 	}
